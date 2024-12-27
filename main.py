61eef735679d6db9a42198136554c5de2bbdaae4
David Gómez Lozano <125882624+DavidGom1@users.noreply.github.com>
2024-12-27 11:00:27 +0100

    creacion contenido para empezar explorador

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from telegram.ext import ApplicationBuilder, MessageHandler, CommandHandler, filters
 from config_data import config #importamos el archivo de configuracion, donde se encuentra el token del bot y los administradores del mismo
 from core import commands #importamos los archivos de las funciones que se ejecutaran en el bot
+from core.archive import Explorador
 import warnings, logging
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -16,25 +17,7 @@ if __name__ == '__main__':
 
     application.add_handler(CommandHandler('help', commands.help))
 
+    recetas = Explorador().conversacion_explorador()
+    application.add_handler(recetas)
+
     application.run_polling()   #iniciamos la aplicacion
-
-    # #creamos la aplicacion
-
-    # jobqueue = application.job_queue
-
-    # recetas = Explorador().conversacion_explorador()
-    # application.add_handler(recetas)  #agregamos el manejador de la conversacion
-
-    # application.add_handler(CommandHandler('calendario', commands.calendario))  #agregamos el manejador del comando calendario
-
-    # application.add_handler(CommandHandler('lista_compra', commands.lista_compra))  #agregamos el manejador del comando calendario
-
-    # application.add_handler(CommandHandler('lista_compra_l_x_keep', commands.lista_compra_L_X_keep))  #agregamos el manejador del comando calendario
-
-    # application.add_handler(CommandHandler('lista_compra_j_d_keep', commands.lista_compra_J_D_keep))  #agregamos el manejador del comando calendario
-
-    # application.add_handler(CommandHandler('prueba', Calend.handle_gen_comidas_cenas))  #agregamos el manejador del comando ayuda
-
-    # application.add_handler(MessageHandler(filters.TEXT, echo.echo))  #agregamos el manejador de mensajes de texto
-
-    # jobqueue.run_repeating(Calend.gen_calend, interval=59)  # Intervalo de una semana (604800 segundos)
